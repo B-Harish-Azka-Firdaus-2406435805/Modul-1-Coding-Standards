cef491e02f8fbeeded5b0f10b9c30517c6387ecc
azkahaf <harishazka@gmail.com>
2026-03-11 05:58:30 +0700

    [GREEN] Implement Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -13,11 +13,49 @@ public class Order {
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
+
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        boolean valid = false;
+        for (String s : statusList) {
+            if (s.equals(status)) {
+                valid = true;
+                break;
+            }
+        }
+        if (!valid) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 
     public void setStatus(String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        boolean valid = false;
+        for (String s : statusList) {
+            if (s.equals(status)) {
+                valid = true;
+                break;
+            }
+        }
+        if (!valid) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
