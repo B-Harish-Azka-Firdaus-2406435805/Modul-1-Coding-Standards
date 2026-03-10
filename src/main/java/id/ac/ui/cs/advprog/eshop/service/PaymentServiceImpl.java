9e409413b90d60ea00219082a280582948fffd75
azkahaf <harishazka@gmail.com>
2026-03-11 06:09:16 +0700

    [GREEN] Implement PaymentServiceImpl class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -15,14 +16,30 @@ public class PaymentServiceImpl implements PaymentService {
     private PaymentRepository paymentRepository;
 
     @Override
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) { return null; }
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, paymentData, order);
+        paymentRepository.save(payment);
+        return payment;
+    }
 
     @Override
-    public Payment setStatus(Payment payment, String status) { return null; }
+    public Payment setStatus(Payment payment, String status) {
+        payment.setStatus(status);
+        if ("SUCCESS".equals(status)) {
+            payment.getOrder().setStatus("SUCCESS");
+        } else if ("REJECTED".equals(status)) {
+            payment.getOrder().setStatus("FAILED");
+        }
+        return paymentRepository.save(payment);
+    }
 
     @Override
-    public Payment getPayment(String paymentId) { return null; }
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
 
     @Override
-    public List<Payment> getAllPayments() { return null; }
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
 }
